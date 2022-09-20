9f760618285ee59256a11e811774cb7697398513
kostili-tec <weh4teyou@gmail.com>
2022-09-20 23:29:11 +0300

    feat: complite pets section

--- online-zoo/src/index.js
+++ online-zoo/src/index.js
@@ -1,2 +1,26 @@
 import './normalize.css';
 import './sass/main.scss';
+
+const petsSection = document.querySelector('.section-pets');
+
+
+petsSection.addEventListener('mouseover', (e) => {
+  const el = e.target; 
+  const related = e.relatedTarget;
+  if (el.classList.contains('pets-img')) {
+    const card = el.parentNode.parentNode;
+    card.style.backgroundColor = 'rgba(0, 0, 0, 0.07)';
+    const description = el.parentNode.parentNode.querySelector('.pets-descpiption');
+    description.style.transform = 'translateY(0px)';
+  }
+})
+
+petsSection.addEventListener('mouseout', (e) => {
+  const el = e.target; 
+  if ( el.classList.contains('pets-img') ) {
+    const card = el.parentNode.parentNode;
+    card.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+    const description = el.parentNode.parentNode.querySelector('.pets-descpiption');
+    description.style.transform = 'translateY(-55px)';
+  }
+})
