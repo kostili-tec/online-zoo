b4cade3722b5bd8562c9380ee06562711f990daf
kostili-tec <weh4teyou@gmail.com>
2022-09-24 18:00:06 +0300

    fix: pets cards show info on hover

--- online-zoo/src/index.js
+++ online-zoo/src/index.js
@@ -3,6 +3,7 @@ import './sass/main.scss';
 
 const petsSection = document.querySelector('.section-pets');
 
+
 const inputBorder = document.querySelector('.input-border');
 const input = document.querySelector('.footer-input');
 const inputButton = document.querySelector('.input-button');
@@ -21,7 +22,6 @@ function onInput() {
     inputButton.classList.remove('input-button__invalid');
     inputButton.classList.add('input-button__default');
     inputButton.classList.add('input-button__hover');
-    // input.style.borderColor = 'green';
 	} else {
     inputBorder.classList.add('input-border__invalid');
     inputBorder.classList.remove('input-border__valid');
@@ -36,20 +36,26 @@ input.addEventListener('input', onInput);
 petsSection.addEventListener('mouseover', (e) => {
   const el = e.target; 
   const related = e.relatedTarget;
-  if (el.classList.contains('pets-img')) {
-    const card = el.parentNode.parentNode;
-    card.style.backgroundColor = 'rgb(224, 223, 223)';
-    const description = el.parentNode.parentNode.querySelector('.pets-descpiption');
-    description.style.transform = 'translateY(0px)';
+  if (el.classList.contains('pets-img') || el.classList.contains('pets-p__info')
+  || el.classList.contains('pets-view__card') || el.classList.contains('pets-descpiption') 
+  || el.classList.contains('pets-h3') || el.classList.contains('pets-p') ) {
+    const infoAnimal = el.parentNode.parentNode.querySelector('.pets-p__info');
+    const imgAnimal = el.parentNode.parentNode.querySelector('.pets-img');    
+ 
+    infoAnimal.classList.add('pets-p__info-show');
+    imgAnimal.classList.add('pets-img__hover');    
   }
 })
 
 petsSection.addEventListener('mouseout', (e) => {
   const el = e.target; 
-  if ( el.classList.contains('pets-img') ) {
-    const card = el.parentNode.parentNode;
-    card.style.backgroundColor = 'rgb(255, 255, 255)';
-    const description = el.parentNode.parentNode.querySelector('.pets-descpiption');
-    description.style.transform = 'translateY(-55px)';
+  if (el.classList.contains('pets-img') || el.classList.contains('pets-p__info')
+  || el.classList.contains('pets-view__card') || el.classList.contains('pets-descpiption')) {
+    const infoAnimal = el.parentNode.parentNode.querySelector('.pets-p__info');
+    const imgAnimal = el.parentNode.parentNode.querySelector('.pets-img');
+    
+    infoAnimal.classList.remove('pets-p__info-show');
+    imgAnimal.classList.remove('pets-img__hover');
+
   }
 })
