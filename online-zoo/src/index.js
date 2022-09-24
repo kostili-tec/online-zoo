3b45523244ecda20fb62f1c992c7cbba94f8111e
kostili-tec <weh4teyou@gmail.com>
2022-09-24 17:02:39 +0300

    feat: complite footer(not working transition on social links)

--- online-zoo/src/index.js
+++ online-zoo/src/index.js
@@ -3,6 +3,35 @@ import './sass/main.scss';
 
 const petsSection = document.querySelector('.section-pets');
 
+const inputBorder = document.querySelector('.input-border');
+const input = document.querySelector('.footer-input');
+const inputButton = document.querySelector('.input-button');
+
+const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+
+function isEmailValid(value) {
+  return EMAIL_REGEXP.test(value);
+}
+
+function onInput() {
+	if (isEmailValid(input.value)) {
+    inputBorder.classList.remove('input-border__invalid');
+    inputBorder.classList.add('input-border__valid');
+    input.classList.remove('footer-input__invalid');
+    inputButton.classList.remove('input-button__invalid');
+    inputButton.classList.add('input-button__default');
+    inputButton.classList.add('input-button__hover');
+    // input.style.borderColor = 'green';
+	} else {
+    inputBorder.classList.add('input-border__invalid');
+    inputBorder.classList.remove('input-border__valid');
+    input.classList.add('footer-input__invalid');
+    inputButton.classList.add('input-button__invalid');
+    inputButton.classList.remove('input-button__hover');
+	}
+}
+
+input.addEventListener('input', onInput);
 
 petsSection.addEventListener('mouseover', (e) => {
   const el = e.target; 
