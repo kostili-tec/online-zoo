6ae34870e570e9b3c027d656edb2bc6fa3030f2c
kostili-tec <weh4teyou@gmail.com>
2022-09-25 23:12:24 +0300

    feat: events for donate inputs

--- online-zoo/src/pages/donate/donate.js
+++ online-zoo/src/pages/donate/donate.js
@@ -1,4 +1,32 @@
-
-
 const moneyInputs = document.querySelectorAll('.feed-contant__radio-input');
-console.log(moneyInputs);
+const moneyLabel = document.querySelectorAll('.radio-value__number');
+const moneySvg = document.querySelectorAll('.dollar-svg');
+
+const anotherInput = document.querySelector('.form__input-number');
+
+moneyInputs.forEach((input, index) => {
+  input.addEventListener('click', (e) => {
+    moneyLabel.forEach((label) => {
+      if (label.classList.contains('checked-label')) {
+        label.classList.remove('checked-label')
+      }
+    })
+    moneySvg.forEach((svg) => {
+      if (svg.classList.contains('checked-svg')) {
+        svg.classList.remove('checked-svg')
+      }
+    })
+    moneyLabel[index].classList.add('checked-label');
+    moneySvg[index].classList.add('checked-svg');   
+  })
+})
+
+anotherInput.addEventListener('change', function() {
+  let value = parseInt(this.value);
+  if (value > 9999) {
+    this.value = 9999
+  }
+  if (value < 1) {
+    this.value = 1;
+  }
+})
