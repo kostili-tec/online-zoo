b2299fa40548b892a50d2db85a9bf83655875b36
kostili-tec <weh4teyou@gmail.com>
2022-10-09 14:56:45 +0300

    feat: events for inputs on donate page

--- online-zoo/src/pages/donate/donate.js
+++ online-zoo/src/pages/donate/donate.js
@@ -1,32 +1,52 @@
-const moneyInputs = document.querySelectorAll('.feed-contant__radio-input');
-const moneyLabel = document.querySelectorAll('.radio-value__number');
-const moneySvg = document.querySelectorAll('.dollar-svg');
-
-const anotherInput = document.querySelector('.form__input-number');
-
-moneyInputs.forEach((input, index) => {
-  input.addEventListener('click', (e) => {
-    moneyLabel.forEach((label) => {
-      if (label.classList.contains('checked-label')) {
-        label.classList.remove('checked-label')
-      }
-    })
-    moneySvg.forEach((svg) => {
-      if (svg.classList.contains('checked-svg')) {
-        svg.classList.remove('checked-svg')
-      }
-    })
-    moneyLabel[index].classList.add('checked-label');
-    moneySvg[index].classList.add('checked-svg');   
+const removeClass = (elements, nameClass) => {
+  elements.forEach((el) => {
+    if (el.classList.contains(nameClass)) {
+      el.classList.remove(nameClass);
+    }
   })
-})
+}
 
-anotherInput.addEventListener('change', function() {
-  let value = parseInt(this.value);
-  if (value > 9999) {
-    this.value = 9999
-  }
-  if (value < 1) {
-    this.value = 1;
-  }
-})
+const donateEvents = () => {  
+  const moneyInputs = document.querySelectorAll('.feed-contant__radio-input');
+  const moneyLabel = document.querySelectorAll('.radio-value__number');
+  const moneySvg = document.querySelectorAll('.dollar-svg');
+  const anotherInput = document.querySelector('.form__input-number');
+  
+  moneyInputs.forEach((input, index) => {
+    input.addEventListener('click', (e) => {
+      removeClass(moneyLabel, 'checked-label');
+      removeClass(moneySvg, 'checked-svg');
+      moneyLabel[index].classList.add('checked-label');
+      moneySvg[index].classList.add('checked-svg');
+      anotherInput.value = Number(moneyLabel[index].textContent);
+    })
+  })
+  
+  anotherInput.addEventListener('input', function() {
+    let value = parseInt(this.value);
+  
+    if (value > 9999) {
+      this.value = 9999
+    }
+    if (value < 1) {
+      this.value = 1;
+    }
+    removeClass(moneyLabel, 'checked-label');
+    removeClass(moneySvg, 'checked-svg');
+  
+    moneyInputs.forEach((input) => {
+      if (input.checked === true)
+      input.checked = false;
+    })
+  
+    moneyLabel.forEach((label, index) => {
+      if (value === Number(label.textContent)) {
+        moneyInputs[index].checked = true;
+        label.classList.add('checked-label');
+        moneySvg[index].classList.add('checked-svg');
+      } 
+    })  
+  })
+}
+
+donateEvents();
